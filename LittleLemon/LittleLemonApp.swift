7cf103af1ec0882cc55c41972153cba9d9ce3a41
lophiester <gilcharles2022@hotmail.com>
2024-03-20 17:07:24 +0900

    Initial Commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Charles Yamamoto on 2024/03/20.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
